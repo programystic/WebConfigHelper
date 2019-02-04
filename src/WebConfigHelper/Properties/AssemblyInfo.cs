c478fb3a285d17492d7125bbcde1a9371fcbcfad
Peter Edney <peter@programystic.com>
2019-02-04 15:21:35 +0000

    AssemblyInfo update

--- src/WebConfigHelper/Properties/AssemblyInfo.cs
+++ src/WebConfigHelper/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebConfigHelper")]
-[assembly: AssemblyDescription("Helper methods to access web config app settings.")]
+[assembly: AssemblyDescription("Helper methods to access web config app settings")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Programystic")]
 [assembly: AssemblyProduct("WebConfigHelper")]
